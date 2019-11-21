d500ec48a0e142b2401f060e6fe3dd04ad948ebe
lchandelon <lchandelon@iparla.iutbayonne.univ-pau.fr>
2019-11-21 09:24:32 +0100

    ajout des urls entreprises, formations et stages

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -4,16 +4,38 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStagesController extends AbstractController
 {
     /**
-     * @Route("/", name="pro_stages-accueil")
+     * @Route("/", name="pro_stages_accueil")
      */
     public function index()
     {
-        return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'contrôleur ProStage',
-        ]);
+        return new Response("<h1>Bienvenue sur la page d'accueil de Prostages</h1>");
+    }
+	/**
+     * @Route("/entreprises", name="pro_stages_entreprises")
+     */
+    public function entreprises()
+    {
+        return new Response("<h1>Cette page affichera la liste des entreprises proposant un stage</h1>");
+    }
+	
+	/**
+     * @Route("/formations", name="pro_stages_formations")
+     */
+    public function formations()
+    {
+        return new Response("<h1>Cette page affichera la liste des formations de l'IUT</h1>");
+    }
+	
+	/**
+     * @Route("/stages/{id}", name="pro_stages_stages/{id}")
+     */
+    public function stages()
+    {
+        return new Response("<h1>Cette page affichera le descriptif du stage ayant pour identifiant {id}</h1>");
     }
 }
